96b2984379302cd2a465e3cdd86018c42d6a972e
CryptJS13 <jasper@slimmens.nl>
2023-01-17 17:10:01 +0100

    lint

--- src/services/web3/index.js
+++ src/services/web3/index.js
@@ -4,7 +4,7 @@ import BigNumber from 'bignumber.js'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import { BscConnector } from '@binance-chain/bsc-connector'
 import CoinbaseWalletSDK from '@coinbase/wallet-sdk'
-import { LoadConnectKit } from '@ledgerhq/connect-kit-loader';
+import { LoadConnectKit } from '@ledgerhq/connect-kit-loader'
 import mobile from 'is-mobile'
 import { get } from 'lodash'
 import contracts from './contracts'
